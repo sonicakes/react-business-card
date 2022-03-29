1a833a6aa4a5f16a5fc31bd0701a79a82444f447
unknown <soniaparker1207@gmail.com>
2022-03-29 15:08:42 +1100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import Info from "./components/Info"
+
+function App() {
+  return (
+    <div className="container">
+      <Info />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Info.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Info() {
+    return (
+        <div>
+            here is info component
+        </div>
+    )
+}
